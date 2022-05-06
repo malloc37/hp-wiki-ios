cdacc576f25e38d6ee9dd756c71dfda3f2d20977
Besmir Kadrii <kadrii.dev@gmail.com>
2022-05-06 18:52:11 +0200

    remove comment

--- HpWiki/charViewModel.swift
+++ HpWiki/charViewModel.swift
@@ -6,8 +6,7 @@
 //
 
 import Foundation
-// add allow arbitrary loads exceptions to allow http connections
-// project -> info -> add custom keys -> app transport.. -> allow arbitrary -> yes
+
 class ApiCall {
     func fetchData(completion:@escaping ([Character]) -> ()) {
         guard let url = URL(string: "http://hp-api.herokuapp.com/api/characters") else { return }
