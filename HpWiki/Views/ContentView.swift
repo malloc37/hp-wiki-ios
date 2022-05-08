f9418bfeb6bb036187304fc043a3556a068b2e72
Besmir Kadrii <kadrii.dev@gmail.com>
2022-05-08 15:19:59 +0200

    add search

--- HpWiki/Views/ContentView.swift
+++ HpWiki/Views/ContentView.swift
@@ -9,21 +9,51 @@ import SwiftUI
 
 struct ContentView: View {
     @ObservedObject var charsModel = CharactersModel()
+    @State var searchQuery = ""
+    @State var filteredChars: [Character] = []
+    
     
     var body: some View {
         VStack {
             Text("HP Wiki")
                 .font(.title)
             NavigationView {
-                List(charsModel.chars) { char in
+                List(getChars(chars: filteredChars)) { char in
                     NavigationLink(destination: CharacterDetailView(char: char)) {
                       Text(char.name)
                     }
                 }
             }
+            .searchable(text: $searchQuery, prompt: "Search by Character Name")
+            .onChange(of: searchQuery) { _ in
+                filterChars()
+            }
+            .onSubmit(of: .search) {
+                filterChars()
+            }
 
         }
         
+    }
+    
+    func filterChars() {
+        if searchQuery.isEmpty {
+            filteredChars = charsModel.chars
+        } else {
+            // filter the chars by name according to the search query. Ignore case sensitivity when filtering the chars
+            filteredChars = charsModel.chars.filter {
+                $0.name
+                    .localizedCaseInsensitiveContains(searchQuery)
+            }
+        }
+    }
+    
+    func getChars(chars: [Character]) -> [Character] {
+        if chars.isEmpty {
+            return charsModel.chars
+        } else {
+            return chars
+        }
     }
 }
 
